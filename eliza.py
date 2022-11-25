bd5cbe69a8bdd1212680c095d2c0524a84529995
atah <atahmih98@gmail.com>
2022-11-25 14:04:01 -0400

    communication flow established

--- eliza.py
+++ eliza.py
@@ -11,26 +11,22 @@ eliza_chatbot = Chat(pairs, reflections)
 
 ##Refer to Lib -> nltk -> util.py for modifications
 def eliza_chat():
-    print("Psychiatrist\n---------")
-    print("Talk to the program by typing in plain English, using normal upper-")
-    print('and lower-case letters and punctuation.  Enter "quit" when done.')
-    print("=" * 72)
-    print("Hello. My name is Eliza.\nHow are you feeling today?")
-    
-    user_input = ""
-    quit="quit"
-    while user_input != quit:
-            user_input = quit
-            try:
-                user_input = input(">")
-            except EOFError:
-                print(user_input)
-            if user_input:
-                
-                while user_input[-1] in "!.":
-                    user_input = user_input[:-1]               
-                reply = eliza_chatbot.converse(user_input)
-                print(reply)
+    pre_input = input()
+    if pre_input == 'Hi':
+        print("Hello. My name is NAO.\nHow are you feeling today?")
+        user_input = ""
+        quit="quit"
+        while user_input != quit:
+                user_input = quit
+                try:
+                    user_input = input(">")
+                except EOFError:
+                    print(user_input)
+                if user_input:                    
+                    while user_input[-1] in "!.":
+                        user_input = user_input[:-1]               
+                    reply = eliza_chatbot.converse(user_input)
+                    print(reply)
   
         
 
